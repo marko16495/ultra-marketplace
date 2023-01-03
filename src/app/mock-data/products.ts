1707fb55d6138bdc1f9ce3b722d01e92ccdbb8ac
Marko <markovujovic164@gmail.com>
2023-01-04 00:03:36 +0100

    Change images

--- src/app/mock-data/products.ts
+++ src/app/mock-data/products.ts
@@ -1,7 +1,7 @@
 import {InjectionToken} from '@angular/core';
 import {ProductSerialized} from '../models/serialized/product-serialized';
 
-// todo: upload images to imgur
+// images taken from https://randomwordgenerator.com/picture.php
 
 export const INITIAL_PRODUCTS = new InjectionToken<ProductSerialized[]>('INITIAL_PRODUCTS', {
   providedIn: 'root',
@@ -10,73 +10,73 @@ export const INITIAL_PRODUCTS = new InjectionToken<ProductSerialized[]>('INITIAL
       id: 1,
       name: 'Product 1',
       price: 19.99,
-      img: 'https://randompicturegenerator.com/img/dragon-generator/ge9cd89a0d5fcfe1385e543e904d0296c632841f303fad6e419b2772deeb576d4661bbf0d00a8c8e925f2631786dac9a5_640.jpg'
+      img: 'https://i.imgur.com/f2WSLEd.jpg',
     },
     {
       id: 2,
       name: 'Product 2',
       price: 129.99,
-      img: 'https://randompicturegenerator.com/img/dragon-generator/g63e03f04488034be372006d7af0c695d54761095519febc0826f1082c44bdb0a495fa74335635a5614804ecf2e6ce440_640.png'
+      img: 'https://i.imgur.com/xDC7GTM.png',
     },
     {
       id: 3,
       name: 'Product 3',
       price: 39.99,
-      img: 'https://randompicturegenerator.com/img/dragon-generator/g0da0a751f61fa729e6294f9614c6fe387a40a8d469716c21ccbf918b026a3045b6bcc44fac0395d8117fb3a6100f8f59_640.jpg'
+      img: 'https://i.imgur.com/s31OIY5.jpg',
     },
     {
       id: 4,
       name: 'Product 4',
       price: 149.99,
-      img: 'https://randompicturegenerator.com/img/dragon-generator/geccdfe6e80a93e2ad6271ecaddf928767f72d4f4a1ceb3737a39d435b899cc035e82db92b41b51afa8e0d33f92e9547c_640.jpg'
+      img: 'https://i.imgur.com/YfO8pV9.jpg'
     },
     {
       id: 5,
       name: 'Product 5',
       price: 259.90,
-      img: 'https://randompicturegenerator.com/img/dragon-generator/g66b65831a8b61346858bc34069ff9cc7e9c1a9762c7befb0d15346fb74af26aadf82068b6d9240e2f92af3ed0aec8335_640.jpg'
+      img: 'https://i.imgur.com/6T4VRGs.jpg'
     },
     {
       id: 6,
       name: 'Product 6',
       price: 69.99,
-      img: 'https://i.imgur.com/hk1E372.jpg'
+      img: 'https://i.imgur.com/BnxwqYc.jpg'
     },
     {
       id: 7,
       name: 'Product 7',
       price: 379.99,
-      img: 'https://i.imgur.com/hk1E372.jpg'
+      img: 'https://i.imgur.com/R3WVrsa.jpg'
     },
     {
       id: 8,
       name: 'Product 8',
       price: 89,
-      img: 'https://i.imgur.com/hk1E372.jpg'
+      img: 'https://i.imgur.com/19KUG15.jpg'
     },
     {
       id: 9,
       name: 'Product 9',
       price: 599.99,
-      img: 'https://i.imgur.com/hk1E372.jpg'
+      img: 'https://i.imgur.com/01j4yf1.jpg'
     },
     {
       id: 10,
       name: 'Product 10',
       price: 199.99,
-      img: 'https://i.imgur.com/hk1E372.jpg'
+      img: 'https://i.imgur.com/KD4D6Cx.jpg'
     },
     {
       id: 11,
       name: 'Product 11',
       price: 99.99,
-      img: 'https://i.imgur.com/hk1E372.jpg'
+      img: 'https://i.imgur.com/Ivq0YKO.jpg'
     },
     {
       id: 12,
       name: 'Product 12',
       price: 12.99,
-      img: 'https://i.imgur.com/hk1E372.jpg'
+      img: 'https://i.imgur.com/aAYJoQl.jpg'
     },
     {
       id: 13,
